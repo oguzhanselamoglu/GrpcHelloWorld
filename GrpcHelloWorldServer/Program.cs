ef9255c00e20184cd24db3d3d68040f3083c3078
oguzhanselamoglu <oguzhanselamoglu@outlook.com>
2022-12-05 09:04:46 +0300

    HelloWorldService Added

--- GrpcHelloWorldServer/Program.cs
+++ GrpcHelloWorldServer/Program.cs
@@ -1,11 +1,16 @@
+using GrpcHelloWorldServer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddGrpc();
 var app = builder.Build();
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -15,6 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapGrpcService<HelloWorldService>();
 
 var summaries = new[]
 {
---
+++ GrpcHelloWorldServer/Services/HelloWorldService.cs
@@ -0,0 +1,27 @@
+﻿
+using Grpc.Core;
+using GrpcHelloWorldServer.Protos;
+namespace GrpcHelloWorldServer.Services
+{
+    public class HelloWorldService: HelloService.HelloServiceBase
+    {
+        private readonly ILogger<HelloWorldService> _logger;    
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger=logger;
+        }
+        public override Task<HelloResponse> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            string resultMessage = $"Hello {request.Name}";
+            var response = new HelloResponse
+            {
+                Message = resultMessage
+            };
+            return Task.FromResult(response);
+        }
+
+
+
+
+    }
+}
