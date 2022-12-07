b04e4d509ffd06ce31ae19adbc01337037aabc50
oguzhanselamoglu <oguzhanselamoglu@outlook.com>
2022-12-07 22:10:01 +0300

    Client added

---
+++ GrpcHelloWorldClient/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcHelloWorldClient.Protos;
+
+Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = new HelloService.HelloServiceClient(channel);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Hello Client" });
+Console.WriteLine($"Greatings :{reply.Message} ");
+Console.ReadKey();
